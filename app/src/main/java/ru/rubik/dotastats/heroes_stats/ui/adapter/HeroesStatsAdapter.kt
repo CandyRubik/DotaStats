1caebe1a1b7e43359b05c7b139f7b24c275ed190
Владимир Терехин <v.terekhin@g.nsu.ru>
2022-12-20 15:48:27 +0700

    split main activity into 2 screens

--- app/src/main/java/ru/rubik/dotastats/heroes_stats/ui/adapter/HeroesStatsAdapter.kt
+++ app/src/main/java/ru/rubik/dotastats/heroes_stats/ui/adapter/HeroesStatsAdapter.kt
@@ -4,21 +4,18 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
-import ru.rubik.dotastats.databinding.FragmentHeroesStatsBinding
 import ru.rubik.dotastats.databinding.ItemHeroStatBinding
 import ru.rubik.dotastats.heroes_stats.domain.entities.HeroStatsItem
-import kotlin.properties.Delegates
 
 class HeroesStatsAdapter(
     diffCallback: DiffUtil.ItemCallback<HeroStatsItem>,
 ) : ListAdapter<HeroStatsItem, HeroStatViewHolder>(diffCallback) {
 
-    var itemsList: List<HeroStatsItem> by Delegates.observable(
-        initialValue = listOf(),
-        onChange = { _, _, newValue ->
-            submitList(newValue)
+    var itemsList: List<HeroStatsItem> = listOf()
+        set(value) {
+            field = value
+            submitList(value)
         }
-    )
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HeroStatViewHolder {
         val binding =
